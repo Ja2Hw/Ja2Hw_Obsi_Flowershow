8da4d2d581cebd96b24ec9b5ef3c2ec9f15805a4
JaeHwan Kim <86146209+Ja2Hw@users.noreply.github.com>
2024-12-06 14:28:35 +0900

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Ja2Hw Obsi Blog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
